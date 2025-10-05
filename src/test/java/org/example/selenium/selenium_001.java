f7496c74240efbb7b49e132e93501e6409e0b12f
NEELIMA <reddineelimacrn@gmail.com>
2025-10-05 17:25:44 +0530

    selenium learning

--- src/test/java/org/example/selenium/selenium_001.java
+++ src/test/java/org/example/selenium/selenium_001.java
@@ -1,4 +1,17 @@
 package org.example.selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+
 public class selenium_001 {
+    public static void main(String[] args) {
+        ChromiumDriver driver= new ChromeDriver();
+        driver.get("https://www.google.com/");
+
+    }
+
+
+
+
 }
